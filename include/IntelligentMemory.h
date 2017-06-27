7d27b3d218c82c38409f82a0c7f59790c25a77e2
wusidn <wusidn@163.com>
2017-06-27 18:07:43 +0800

    2017.27.06

---
+++ include/IntelligentMemory.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef __INTELLIGENT_MEMORY_H__
+#define __INTELLIGENT_MEMORY_H__
+
+#include "../src/baseObj.h"
+
+
+#endif //__INTELLIGENT_MEMORY_H__
---
+++ src/gc.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef __GC_H__
+#define __GC_H__
+
+#include "baseObj.h"
+
+namespace ROOT_SPACE
+{
+    class gc : public baseObj
+    {
+    public:
+        static 
+    };
+}
+
+#endif //__GC_H__
